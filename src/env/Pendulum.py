6155b562c3a7de0aafc3e5c0fd0893aa4ccc804d
CTeufel <cteufel@ethz.ch>
2024-05-01 20:16:40 +0200

    changed reward func

--- src/env/Pendulum.py
+++ src/env/Pendulum.py
@@ -79,7 +79,8 @@ class InvertedPendulum(PipelineEnv):
     obs = self._get_obs(pipeline_state)
     target= state.target
     wp,wx,wv,wa = 1,1,1,1
-    reward = -wp*(obs[1])**2 - wx*(jp.cos(obs[0])**2 + (obs[0]-target + jp.sin(obs[1]))**2) - wa*(action)**2 - wv*(obs[2]**2 + obs[3]**2)
+   # - wx*(jp.cos(obs[0])**2 + (obs[0]-target + jp.sin(obs[1]))**2)
+    reward = -wp*(obs[1])**2 - wa*(action)**2 - wv*(obs[2]**2 + obs[3]**2)
     reward =jp.array(reward[0],float)
     done = jp.where(jp.abs(obs[1]) > 0.2, 1.0, 0.0)
     return state.replace(
