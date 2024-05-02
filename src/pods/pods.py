6987b8cb1ab9af2694272088d90d7e65589769dc
dcsteinhauser <dsteinhauser@ethz.ch>
2024-05-02 11:40:55 +0200

    cleaned up for main

--- src/pods/pods.py
+++ src/pods/pods.py
@@ -16,15 +16,13 @@ from src.env.Pendulum import State
 import time 
 from flax import serialization
 import pickle
+
 @flax.struct.dataclass
 class TrainState:
     policy_model: DeterministicPolicy
     policy_params: Params
     optimizer_state: optax.OptState
     optimizer: optax.GradientTransformation
-
-
-
 
 def generate_trajectory(environment, train_state: TrainState, trajectory_length: int, num_samples, prng_keys: PRNGKey):
     
@@ -51,8 +49,6 @@ def generate_trajectory_parallel(environment, train_state: TrainState, trajector
 
     state: State = environment.reset(prng_keys)
     _, (updatedstates, updatedactions,reward) = jax.lax.scan(step_trajectory, state, xs=None, length=trajectory_length)
-    print(updatedstates.shape, updatedactions.shape)
-    print("HALLO")
     states=jax.numpy.reshape(updatedstates, (num_samples, trajectory_length, environment.observation_size))
     actions=jax.numpy.reshape(updatedactions, (num_samples, trajectory_length, environment.action_size))
     totalreward=jnp.sum(reward, axis=0)
@@ -77,20 +73,6 @@ def fo_update_action_sequence(environment, actions, prng_key, alpha_a):
     return improved_action_sequence
 
 
-"""def mse_loss(inputs,targets):
-    diff = jax.pure_callback(inputs - targets)
-    print("inputs:")
-    print(inputs)
-    print("targets:")
-    print(targets)
-    print("shapa")
-    print(diff.shape)
-    print(diff)
-    loss= 0.5*jnp.inner(diff,diff)
-    print("loss")
-    print(loss)
-    return loss"""
-
 def update_policy(states, actions, train_state):
     params = train_state.policy_params
     policy_model = train_state.policy_model
@@ -99,16 +81,12 @@ def update_policy(states, actions, train_state):
     
     policy_output_fn = policy_model.apply
     loss_fn = lambda params, states, actions: 0.5*optax.losses.squared_error(policy_output_fn(params,states), actions).mean()
-    # print(loss_fn(params, states, actions))
     value,grad = jax.value_and_grad(loss_fn)(params, states, actions)
     updates, optimizer_state = optimizer.update(grad, optimizer_state)
     new_params = optax.apply_updates(params, updates)
     train_state = train_state.replace(policy_params=new_params, optimizer_state=optimizer_state)
 
     return value, train_state
-
-
-
 
 def train(
     env,
@@ -130,20 +108,19 @@ def train(
     t0= time.time()
 
     # Define the optimizer
-    #optimizer = optax.adam(learning_rate=1e-4)
-
     scheduler = optax.exponential_decay(
-    init_value=1e-5,
-    transition_steps=1000,
-    decay_rate=0.99)
+        init_value=1e-5,
+        transition_steps=1000,
+        decay_rate=0.99)
+    
     # Combining gradient transforms using `optax.chain`.
     optimizer = optax.chain(
-    optax.clip_by_global_norm(1.0),  # Clip by the gradient by the global norm.
-    optax.scale_by_adam(),  # Use the updates from adam.
-    optax.scale_by_schedule(scheduler),  # Use the learning rate from the scheduler.
-    # Scale updates by -1 since optax.apply_updates is additive and we want to descend on the loss.
-    optax.scale(-1.0)
-    )
+        optax.clip_by_global_norm(1.0),  # Clip by the gradient by the global norm.
+        optax.scale_by_adam(),  # Use the updates from adam.
+        optax.scale_by_schedule(scheduler),  # Use the learning rate from the scheduler.
+        # Scale updates by -1 since optax.apply_updates is additive and we want to descend on the loss.
+        optax.scale(-1.0))
+    
     optimizer_state = optimizer.init(policy_params)
 
     # Initialize the training state
@@ -153,8 +130,8 @@ def train(
         optimizer_state=optimizer_state,
         optimizer=optimizer)
     
-    #nonbactehd env
-    non_batched_env= env 
+    # Initialize a nonbatched env
+    non_batched_env = env 
     # Wrap the environment to allow vmapping
     environment = envs.training.wrap(env, episode_length=trajectory_length,)
     
@@ -163,8 +140,8 @@ def train(
     # 3. for each episode initialize an array which has the sequence of actions taken by the policy
     # 4. rerun the environment, using the array of actions as input, and calculate the total reward
     # 5. calculate the gradient of the total reward with respect to the array of aprng_key[0]
-    x_data=[]
-    y_data=[]
+    x_data = []
+    y_data = []
     for i in range(epochs):
         # update rng keys
         key1, key2 = jax.random.split(new_key)
@@ -173,16 +150,15 @@ def train(
         
         # generate trajectories
         trajectories = generate_trajectory_parallel(environment, train_state, trajectory_length, num_samples, subkeys)
-        totalreward=trajectories[2]
-        trajectories=trajectories[:2]
-        print(trajectories[0])
-
-
-        print("Total Reward",jnp.mean(totalreward))
-        # updatedaction sequence
+        total_reward = trajectories[2]
+        trajectories = trajectories[:2]
+        
+        # output progress
+        progress_fn(x_data,y_data,i,jnp.mean(total_reward))
+        
+        # update action sequence
         states, actions = trajectories[0], fo_update_action_sequence(non_batched_env, trajectories[1], subkeys, alpha_a)
         
-        progress_fn(x_data,y_data,i,jnp.mean(totalreward))
         # supervised learning
         for j in range(epochs//3):
             for state_sequence, action_sequence in zip(states, actions):
@@ -191,11 +167,12 @@ def train(
             if(value<1e-4 or value == jnp.nan):
                 break
        
-        
+       # checkpoint
         if i % 10 == 0:    
             params = serialization.to_state_dict(train_state.policy_params)
             with open("params.pkl", "wb") as f:
                 pickle.dump(params, f)
+
     return functools.partial(train_state.policy_model.apply, variables=train_state.policy_params)
 
             
