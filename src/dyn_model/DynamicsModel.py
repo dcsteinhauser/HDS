8f9fb7dd51def6009d97b02565a0ca5ba60e7aa6
CTeufel <cteufel@ethz.ch>
2024-05-20 17:35:18 +0200

    fixed the dynamics model train pipeline

--- src/dyn_model/DynamicsModel.py
+++ src/dyn_model/DynamicsModel.py
@@ -13,18 +13,22 @@ class model(nn.Module):
         self.dense4 = nn.Dense(16)
         self.dense5 = nn.Dense(8)
         self.dense6 = nn.Dense(self.output_size)
+        self.layer_norm = nn.LayerNorm()
+        self.layer_norm2 = nn.LayerNorm()
 
     def __call__ (self, x):
         x = self.dense1(x)
-        x = jax.nn.relu(x)
+        x = self.layer_norm(x)
+        x = jax.nn.leaky_relu(x)
         x = self.dense2(x)
-        x = jax.nn.relu(x)
+        x = jax.nn.leaky_relu(x)
         x = self.dense3(x)
-        x = jax.nn.relu(x)
+        x = jax.nn.leaky_relu(x)
         x = self.dense4(x)
-        x = jax.nn.relu(x)
+        x = jax.nn.leaky_relu(x)
+        x = self.layer_norm2(x)
         x = self.dense5(x)
-        x = jax.nn.relu(x)
+        x = jax.nn.leaky_relu(x)
         x = self.dense6(x)
         return x
 
